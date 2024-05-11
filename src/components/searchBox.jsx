99765685363b72dcfdf6f51992fc41ec7161f7dc
Santosh Yadav <ymanoutube@gmail.com>
2024-05-11 11:16:09 +0545

    search box

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -1,12 +1,14 @@
 const SearchBox = () => {
-    return (
+  return (
+    <div className="  text-center p-4">
       <input
-        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+        className="border-2 border-gray-300  bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
         type="search"
         name="search"
         placeholder="Search"
       />
-    );
+    </div>
+  );
 };
-  
+
 export default SearchBox;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import Card from "../components/Card";
 
 const Home = () => {
   return (
-    <div className="container bg-blue-300 mx-auto ">
+    <div className="container bg-blue-300 mx-auto min-h-screen ">
       <SearchBox />
       <Card />
     </div>
