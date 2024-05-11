7a9eeaf9af232e680d851c022221e15f7a7c7b48
Santosh Yadav <ymanoutube@gmail.com>
2024-05-11 10:36:07 +0545

    tailwind config

---
+++ src/components/searchBox.jsx
@@ -0,0 +1,12 @@
+const SearchBox = () => {
+    return (
+      <input
+        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+        type="search"
+        name="search"
+        placeholder="Search"
+      />
+    );
+};
+  
+export default SearchBox;
